3ab6042a5291a2e2a9311e4a5bc1b6f6af6778ea
moonsea <moonsea3x3@outlook.com>
2018-12-17 16:57:33 +0800

    配置项目的文件

---
+++ app.js
@@ -0,0 +1,11 @@
+//1.导包
+const express= require('express');
+const router = require('./router');
+//2.配置
+const app = express();
+//3.使用路由
+app.use(router);
+//4.监听端口
+app.listen(4399,()=>{
+    console.log("yaho— — —")
+})
